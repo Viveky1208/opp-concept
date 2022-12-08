dfcedaf0f1ceaee1ca54c307ef5a72f889386867
Viveky1208 <vivekyadav120802>
2022-12-08 09:25:29 +0530

    program

---
+++ concanate Operator Overloading.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+class a
+{
+	public : void set ()
+	{
+		cout<<"hello";
+    }
+};
+class b
+{
+	public : void set ()
+	{
+		cout<<"world";
+	 }
+};
+void main()
+{
+	a obj;
+	b obj;
+	ob.set();
+	obj.set();
+}
